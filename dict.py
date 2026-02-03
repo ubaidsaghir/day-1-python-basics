708ad1a0fcdd04d34999770ac3bb49c5516c1355
ubaidsaghir <105512845+ubaidsaghir@users.noreply.github.com>
2026-02-03 19:26:06 +0500

    Python Dictionaries basics

---
+++ dict.py
@@ -0,0 +1,49 @@
+dic = {
+    1: "Ubaid",
+    2: "Ahsan",
+    344:"ali",
+    56:"Hassan"
+}
+print(dic.values())
+for key in dic.keys():
+ print(f"the value corresponding to the key {key} is{dic[key]}")
+print(dic.items())
+
+students = [
+    {"name":"Ali","gpa":3.6},
+    {"name":"Ahsan","gpa":3.8},
+    {"name":"Umer","gpa":2.6},
+    {"name":"Adeel","gpa":2.9},
+    {"name":"hassan","gpa":3.4},
+]
+
+for student in students:
+    if student["gpa"] > 3.0:
+        print(student["name"])
+
+students = [
+    {"name":"Ubaid","marks":90},
+    {"name":"Ali","marks":50},
+    {"name":"Ahmad","marks":77},
+    {"name":"Umer","marks":88},
+    {"name":"Usman","marks":60},
+]
+
+for student in students:
+    if student ["marks"] > 70:
+        print(student["name"])
+        
+
+products = [
+    
+    {"name":"laptop", "price":70000,"quantity":5},
+    {"name":"mouse", "price":4000,"quantity":10},
+    {"name":"keyboard", "price":10000,"quantity":0},
+]
+
+for product in products:
+    if product["price"] <50000:
+        print(product["name"],"Out of Stock")
+    else:
+        print(product["name"],"In Stock")    
+            
